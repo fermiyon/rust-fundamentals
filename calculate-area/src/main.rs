95c78487c12bb2cc97d66263dff8487d2ee0236c
Selman <selmank@gmail.com>
2024-06-09 15:35:45 +0200

    Update calculate area: add new circle

--- calculate-area/src/main.rs
+++ calculate-area/src/main.rs
@@ -45,24 +45,25 @@ fn main() {
     let shapes = vec![
         Shape::Circle(Circle { radius: 5.0 }),
         Shape::Square(Square { length: 3.0 }),
+        Shape::Circle(Circle { radius: 2.0 }),
     ];
 
+    // The collect::<Result<Vec<_>, _>>() method attempts to collect the transformed
+    // items into a Result containing a vector of areas. If any of the areas resulted in an error,
+    // the entire collection will result in an Err
+    //
+    // The areas inside the map function, after collect, is a Vec<f64> only if the Result is Ok.
+    // To sum up the elements of this vector, we need to turn it into an iterator first,
+    // with the areas.iter(). The iter method borrows each element of the collection,
+    // allowing to use them to calculate the sum without taking ownership of the vector.
     let total_area: Result<f64, &'static str> = shapes
         .iter()
         .map(|shape| match shape {
             Shape::Circle(circle) => circle.area(),
             Shape::Square(square) => square.area(),
         })
-        .collect::<Result<Vec<_>, _>>() 
+        .collect::<Result<Vec<_>, _>>()
         .map(|areas| areas.iter().sum()); // Sum the areas if they are all valid
-    //! The collect::<Result<Vec<_>, _>>() method attempts to collect the transformed 
-    //! items into a Result containing a vector of areas. If any of the areas resulted in an error, 
-    //! the entire collection will result in an Err
-    //! 
-    //! The areas inside the map function, after collect, is a Vec<f64> only if the Result is Ok. 
-    //! To sum up the elements of this vector, we need to turn it into an iterator first, 
-    //! with the areas.iter(). The iter method borrows each element of the collection, 
-    //! allowing to use them to calculate the sum without taking ownership of the vector.
 
     match total_area {
         Ok(area) => println!("Total area: {:.2} sq. units", area),
